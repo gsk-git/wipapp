bae851a82ee4d1f4719f11963aafe6f1ee542e7c
gsk-git <gauthamsevar@gmail.com>
2025-01-21 23:45:44 +0530

    New push

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ This module creates a Flask application that renders an HTML template.
 It defines a root route ('/') that serves the 'index.html' template.
 """
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 # Define root route for this app
